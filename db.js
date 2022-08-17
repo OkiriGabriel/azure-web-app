b88ae56f99dfc9dca21cedfb71dabedc6669a161
okirigabriel <okirigabriel24@gmail.com>
2022-08-17 17:23:09 -0400

    EDIT2

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "okiri",
     host: "okiri.postgres.database.azure.com",
-    database: "okiri",
+    dbname: "okiri",
     password: "Micheal112@",
     port: 5432
 });
