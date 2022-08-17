4996a64496561af5e0d2cf4316ce41b967097abc
okirigabriel <okirigabriel24@gmail.com>
2022-08-17 16:40:58 -0400

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const studentRoutes = require('./src/student/routes');
 
 const app = express();
-const port = 3000;
+const port = 5432;
 
 app.use(express.json());
 
